a8ebeebeeacc25e5f87e63ed67de179bbc08d39e
Josh Martin <joshualmartin@gmail.com>
2017-03-02 12:27:51 -0800

    Added a bit of info.

--- 7zipExtractor.ps1
+++ 7zipExtractor.ps1
@@ -1,7 +1,10 @@
+# This piece of PowerShell will examine a directory, filter out the parts of a multi-part .rar archive and then begin the extraction job on the primary file.
+# it will then move on to the next primary and begin again while displaying progress in the shell.
+
 
 #This will get the list of files within each folder recursively
 
-$filelist = Get-ChildItem -Path G:\Movies\ -Recurse | Where-Object {$_.name -like "*.rar" -and $_.name -notlike "*part0*.rar" -and $_.name -notlike "*part1*.rar" -and $_.name -notlike "*subs-saints.rar"}
+$filelist = Get-ChildItem -Path G:\folder\ -Recurse | Where-Object {$_.name -like "*.rar" -and $_.name -notlike "*part0*.rar" -and $_.name -notlike "*part1*.rar" -and $_.name -notlike "*sub.rar"}
 $count = $null
 
 #Extract each compressed archive
